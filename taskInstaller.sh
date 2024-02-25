dcf561ba61abdd486f44fa8f3971a4b82550928b
ACanoSJO <acanov62@alumnes.santjosepobrer.es>
2024-02-25 19:27:10 +0100

    ASO-final v1.1.0

--- taskInstaller.sh
+++ taskInstaller.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /usr/local/bin
-sudo wget "https://github.com/ACanoSJO/ASO-final/blob/main/taskManager.zip"
-sudo apt install unzip
+sudo wget "https://github.com/ACanoSJO/ASO-final/raw/main/taskManager.zip"
+sudo apt install zip unzip
 sudo unzip taskManager.zip
-alias taskManager="/usr/local/bin/taskManager/start.sh"
+sudo alias taskManager="sudo sh /usr/local/bin/taskManager/start.sh"
+sudo chmod -R +x /usr/local/bin/taskManager
+sudo rm /usr/local/bin/taskManager.zip
