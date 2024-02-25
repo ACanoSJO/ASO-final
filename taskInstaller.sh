bdc93ba02b1a8cecdb30a0c5716ec20a6463ced1
ACanoSJO <acanov62@alumnes.santjosepobrer.es>
2024-02-25 18:21:49 +0100

    ASO-final v1.0.1

--- taskInstaller.sh
+++ taskInstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /usr/local/bin
-curl -JLO "https://github.com/ACanoSJO/proyecto-ASO-task-manager/taskManager.zip"
-unzip taskManager.zip
+wget "https://github.com/ACanoSJO/ASO-final/blob/main/taskManager.tar"
+tar -xvf taskManager.tar
 alias taskManager="/usr/local/bin/taskManager/start.sh"
